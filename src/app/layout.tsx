2c5c3d7174688bba819a292ada458b1fae947c9f
suman-chandra-barman <sumanchandrabarman999@gmail.com>
2026-04-17 11:19:24 +0600

    Add local font mulish and minionPro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,54 @@
 import type { Metadata } from "next";
-import { Mulish } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const mulish = Mulish({
+// Mulish Font Configuration
+const mulish = localFont({
+  src: [
+    {
+      path: "../../public/fonts/mulish/Mulish-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/mulish/Mulish-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/mulish/Mulish-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/mulish/Mulish-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
   variable: "--font-mulish",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+});
+
+// Minion Pro Font Configuration
+const minionPro = localFont({
+  src: [
+    {
+      path: "../../public/fonts/minion/MinionPro-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/minion/MinionPro-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/minion/MinionPro-It.otf",
+      weight: "400",
+      style: "italic",
+    },
+  ],
+  variable: "--font-minion",
 });
 
 export const metadata: Metadata = {
@@ -21,7 +64,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${mulish.variable} h-full antialiased`}
+      className={`${mulish.variable} ${minionPro.variable} font-sans h-full antialiased`}
       suppressHydrationWarning
     >
       <body
