b92f354bfd807671cffcfc7b9e0be5db1d083b80
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2026-05-21 10:51:06 +0600

    Update CategoriesPanelSkeleton to include additional skeleton items for improved loading representation

--- src/components/skeleton/CategoriesPanelSkeleton.tsx
+++ src/components/skeleton/CategoriesPanelSkeleton.tsx
@@ -3,7 +3,7 @@ function CategoriesPanelSkeleton() {
     <div className="mt-8">
       <div className="h-4 w-24 rounded bg-[#e6ded8] animate-pulse" />
       <div className="mt-4 grid gap-3">
-        {[0, 1, 2, 3].map((item) => (
+        {[0, 1, 2, 3,4, 5, 6].map((item) => (
           <div
             key={item}
             className="h-4 w-40 rounded bg-[#e6ded8] animate-pulse"
@@ -12,7 +12,7 @@ function CategoriesPanelSkeleton() {
       </div>
 
       <div className="mt-6 grid grid-cols-3 gap-2">
-        {[0, 1, 2, 3, 4, 5, 6, 7].map((item) => (
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
           <div
             key={item}
             className="aspect-3/4 w-full rounded-[0.42rem] bg-[#e6ded8] animate-pulse"
